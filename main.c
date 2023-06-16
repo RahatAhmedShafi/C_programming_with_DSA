4a09f96589ab639a043aa01750e1bfc43ceb87d2
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-06-16 11:37:06 +0600

    Exercise

--- main.c
+++ main.c
@@ -347,8 +347,8 @@
 //        int j,key;
 //
 //        for(int i=1;i<=n-1;i++){
-//            key=arr[i];
-//            j=i-1;
+//            key=arr[i]; // key=2
+//            j=i-1;  // j=4
 //
 //            while(j>=0 && arr[j]>key){
 //                    arr[j+1]=arr[j];
@@ -572,7 +572,8 @@
 //    }
 
 
-            // Binary Search
+           //  Binary Search
+//
 //    int BinaryFunction(int *arr,int size,int element){
 //        int low=0;
 //        int high=size-1;
@@ -605,45 +606,484 @@
 
             /* Ternary Search*/
 
-    int TernaryFunction(int *arr,int size,int element){
-        int l=0;
-        int r=size-1;
-        while(l<=r){
-            int mid1=l+((r-l)/3);
-            int mid2=r-(r-l)/3;
-            if(element==arr[mid1]) {
-                return mid1;
+//    int TernaryFunction(int *arr,int size,int element){
+//        int l=0;
+//        int r=size-1;
+//        while(l<=r){
+//            int mid1=l+((r-l)/3);
+//            int mid2=r-(r-l)/3;
+//            if(element==arr[mid1]) {
+//                return mid1;
+//            }
+//            if(element==arr[mid2]){
+//                return mid2;
+//            }
+//            if(element<arr[mid1]){
+//                r=mid1-1;
+//            }
+//            if(element>arr[mid2]){
+//                l=mid2+1;
+//            }
+//            else{
+//                l=mid1+1;
+//                r=mid2-1;
+//            }
+//        }
+//        return -1;
+//    }
+//    int main(){
+//        int arr[]={10,20,30,40,50,60};
+//        int size=sizeof(arr)/sizeof(int);
+//        int element=60;
+//        int TernarySearch=TernaryFunction(arr,size,element);
+//        printf("%d",TernarySearch);
+//    }
+
+        // Linear Search
+
+//    int TernaryFunction(int *arr,int size,int element){
+//        int low=0;
+//        int high=size-1;
+//        while(low<=high){
+//           int mid1=low+((low+high)/3);
+//           int mid2=high-((low+high)/3);
+//           if(arr[mid1]==element){
+//                return mid1;
+//           }
+//           if(arr[mid2]==element){
+//                return mid2;
+//           }
+//           if(arr[mid1]>element){
+//                low=mid1+1;
+//           }
+//           if(arr[mid2]<element){
+//                high=mid2-1;
+//           }
+//           else{
+//                low=mid1+1;
+//                high=mid2-1;
+//           }
+//
+//        }
+//        return -1;
+//
+//    }
+//    int main(){
+//        int arr[]={10,20,40,80,90};
+//        int size=sizeof(arr)/sizeof(int);
+//        int element;
+//        scanf("%d",&element);
+//        int TernarySearch= TernaryFunction(arr,size,element);
+//        printf("%d",TernarySearch);
+//    }
+
+        // Bubble Sort
+//    void PrintFunction(int *arr,int size){
+//        for(int i=0;i<size;i++){
+//            printf("%d ",arr[i]);
+//        }
+//        printf("\n");
+//    }
+//    void BubbleFunction(int *arr,int size){
+//        for(int i=0;i<size-1;i++){
+//            for(int j=0;j<size-1-i;j++){
+//                if(arr[j]>arr[j+1]){
+//                    int temp=arr[j];
+//                    arr[j]=arr[j+1];
+//                    arr[j+1]=temp;
+//                }
+//            }
+//        }
+//    }
+//    int main(){
+//        int arr[]={4,2,17,1,8,5};
+//        int size=sizeof(arr)/sizeof(int);
+//        PrintFunction(arr,size);
+//        BubbleFunction(arr,size);
+//        PrintFunction(arr,size);
+//    }
+
+            // Insertion Sort
+
+//    void PrintFunction(int *arr,int size){
+//        for(int i=0;i<size;i++){
+//            printf("%d ",arr[i]);
+//        }
+//        printf("\n");
+//    }
+//    void InsersionFunction(int *arr,int size){
+//        for(int i=1;i<size;i++){
+//            int key=arr[i];
+//            int j=i-1;
+//            while(j>=0 && arr[j]>key){
+//                arr[j+1]=arr[j];
+//                j--;
+//            }
+//            arr[j+1]=key;
+//        }
+//    }
+//    int main(){
+//        int arr[]={4,2,17,1,8,5};
+//        int size=sizeof(arr)/sizeof(int);
+//        PrintFunction(arr,size);
+//        InsersionFunction(arr,size);
+//        PrintFunction(arr,size);
+//    }
+
+
+        // Selection Sort
+
+//     void PrintFunction(int *arr,int size){
+//        for(int i=0;i<size;i++){
+//            printf("%d ",arr[i]);
+//        }
+//        printf("\n");
+//    }
+//    void SelectionFunction(int *arr,int size){
+//        for(int i=0;i<size;i++){
+//            int IndexMin=i;
+//            for(int j=i+1;j<size;j++){
+//                if(arr[j]<arr[IndexMin]){
+//                    IndexMin=j;
+//                }
+//            }
+//            int temp=arr[i];
+//            arr[i]=arr[IndexMin];
+//            arr[IndexMin]=temp;
+//        }
+//    }
+//    int main(){
+//        int arr[]={4,2,17,1,8,5};
+//        int size=sizeof(arr)/sizeof(int);
+//        PrintFunction(arr,size);
+//        SelectionFunction(arr,size);
+//        PrintFunction(arr,size);
+//    }
+
+        // Linear Search
+//    int FoundElement(int *arr,int size,int element){
+//        for(int i=0;i<size;i++){
+//            if(arr[i]==element){
+//               return i;
+//            }
+//        }
+//       return -1;
+//
+//    }
+//    int main(){
+//        int arr[]={4,2,17,1,8,5};
+//        int size=sizeof(arr)/sizeof(int);
+//        int element;
+//        scanf("%d",&element);
+//        int Indexprint=FoundElement(arr,size,element);
+//        printf("%d",Indexprint);
+//
+//    }
+
+        // Linear Search (For Multiple Indices)
+
+//#define SIZE 10000
+//
+//struct Result
+//{
+//    int numberOfMatches;
+//    int indices[SIZE];
+//};
+//
+////void linearSearch(int arr[], int n, int key)
+////{
+////    for(int i=0; i<n; i++)
+////    {
+////        if(arr[i]==key)
+////            printf("%d ", i);
+////    }
+////    printf("\n");
+////}
+//
+//struct Result linearSearchFunctional(int arr[], int n,  int key)
+//{
+//   struct Result result;
+//    result.numberOfMatches = 0;
+//
+//    for(int i=0; i<n; i++)
+//    {
+//        if(arr[i]==key)
+//        {
+//            result.indices[result.numberOfMatches] = i;
+//            result.numberOfMatches++;
+//        }
+//    }
+//    return result;
+//}
+//
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    int arr[n];
+//    for(int i=0; i<n; i++)
+//        scanf("%d", &arr[i]);
+//    int key;
+//    scanf("%d", &key);
+// struct  Result result = linearSearchFunctional(arr, n, key);
+//    for(int i=0; i<result.numberOfMatches; i++)
+//    {
+//        printf("%d ", result.indices[i]);
+//    }
+//    return  0;
+//}
+
+
+    // Binary Search (Descending Order)
+
+//    int BinaryFunction(int *arr,int n,int key){
+//        int low=0;
+//        int high=n-1;
+//        while(low<=high){
+//            int mid=((low+high)/2);
+//            if(arr[mid]==key){
+//                return mid;
+//            }
+//            if(arr[mid]>key){
+//                low=mid+1;
+//            }
+//            else{
+//                high=mid-1;
+//            }
+//
+//
+//        }
+//        return -1;
+//    }
+//    int main(){
+//        int n;
+//        scanf("%d",&n);
+//        int arr[n];
+//        for(int i=0;i<n;i++){
+//            scanf("%d",&arr[i]);
+//        }
+//        int key;
+//        scanf("%d",&key);
+//        int BinarySearch=BinaryFunction(arr,n,key);
+//        if(BinarySearch==-1)
+//        printf("Not Found\n");
+//        else
+//        printf("Found at index: %d\n",BinarySearch);
+//
+//
+//    }
+
+        // Searching & Sorting : Find first and last positions of an element in a sorted array
+//    #define SIZE 10000
+//
+//    struct Result{
+//        int NumberOfMatches;
+//        int indeces[SIZE];
+//    };
+//
+//    struct Result SearchIndex(int *arr,int n,int element){
+//        struct Result result;
+//        result.NumberOfMatches=0;
+//        for(int i=0;i<n;i++){
+//            if(arr[i]==element){
+//                result.indeces[result.NumberOfMatches]=i;
+//                result.NumberOfMatches++;
+//                break;
+//
+//            }
+//        }
+//        for(int i=n-1;i>0;i--){
+//            if(arr[i]==element){
+//                result.indeces[result.NumberOfMatches]=i;
+//                result.NumberOfMatches++;
+//                break;
+//
+//            }
+//        }
+//        return result;
+//
+//    }
+//
+//    int main(){
+//        int n;
+//        scanf("%d",&n);
+//        int arr[n];
+//        for(int i=0;i<n;i++){
+//            scanf("%d",&arr[i]);
+//        }
+//        int element;
+//        scanf("%d",&element);
+//        struct Result result=SearchIndex(arr,n,element);
+//
+//        for(int i=0;i<result.NumberOfMatches;i++){
+//
+//            printf("%d ",result.indeces[i]);
+//
+//
+//        }
+//
+//
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    int BinaryFunction(int *arr,int size,int element){
+//        int low=0;
+//        int high=size-1;
+//        int mid;
+//        while(low<=high){
+//                mid=(low+high)/2;
+//            if(arr[mid]==element){
+//               return mid;
+//            }
+//            if(arr[mid]<element){
+//                low=mid+1;
+//            }
+//            else{
+//                high=mid-1;
+//            }
+//        }
+//        return -1;
+//    }
+//    int main(){
+//        int arr[]={4,5,6,9,9,9,17,18};
+//        int size=sizeof(arr)/sizeof(int);
+//        int element;
+//        scanf("%d",&element);
+//        int BinarySearch=BinaryFunction(arr,size,element);
+//        printf("%d",BinarySearch);
+//    }
+
+
+
+
+
+
+//        #define SIZE 10000
+//
+//        struct Result
+//        {
+//            int numberOfMatches;
+//            int indices[SIZE];
+//        };
+//
+//        //void linearSearch(int arr[], int n, int key)
+//        //{
+//        //    for(int i=0; i<n; i++)
+//        //    {
+//        //        if(arr[i]==key)
+//        //            printf("%d ", i);
+//        //    }
+//        //    printf("\n");
+//        //}
+//
+//        struct Result linearSearchFunctional(int arr[], int n)
+//        {
+//           struct Result result;
+//            result.numberOfMatches = 0;
+//
+//            for(int i=0; i<n; i++)
+//            {
+//                if(arr[i]==i)
+//                {
+//                   result.indices[result.numberOfMatches] = i;
+//                   result.numberOfMatches++;
+//                    return result;
+//                }
+//            }
+////            return result;
+//        }
+//
+//        int main()
+//        {
+//            int n;
+//            scanf("%d", &n);
+//            int arr[n];
+//            for(int i=0; i<n; i++)
+//                scanf("%d", &arr[i]);
+////            int key;
+////            scanf("%d", &key);
+//         struct  Result result = linearSearchFunctional(arr, n);
+////            for(int i=0; i<result.numberOfMatches; i++)
+////            {
+////                printf("%d ", result.indices[i]);
+////            }
+//                for(int i=0;i<result.numberOfMatches;i++){
+//                    printf("%d",);
+//                }
+//            return  0;
+//        }
+
+
+//        void SearchEqualElement(int *arr,int size){
+//            for(int i=1;i<=size;i++){
+//                if(arr[i]==i){
+//
+//                  printf("%d",arr[i]);
+//                }
+//            }
+//
+//        }
+//        int main(){
+//            int arr[]={4,5,6,10,4};
+//            int size=sizeof(arr)/sizeof(int);
+//            int NumberOfMatches=0;
+//           SearchEqualElement(arr,size);
+//
+//        }
+
+
+            //Searching & Sorting :	Find a Fixed Point (Value equal to index) in a given array
+
+                #define SIZE 10000
+
+        struct Result
+        {
+            int numberOfMatches;
+            int indices[SIZE];
+        };
+
+
+
+        struct Result linearSearchFunctional(int arr[], int n)
+        {
+           struct Result result;
+            result.numberOfMatches = 1;
+
+            for(int i=1; i<=n; i++)
+            {
+                if(arr[i]==i)
+                {
+                    result.indices[result.numberOfMatches] = i;
+                    result.numberOfMatches++;
+
+                }
             }
-            if(element==arr[mid2]){
-                return mid2;
-            }
-            if(element<arr[mid1]){
-                r=mid1-1;
-            }
-            if(element>arr[mid2]){
-                l=mid2+1;
-            }
-            else{
-                l=mid1+1;
-                r=mid2-1;
-            }
+             return result;
         }
-        return -1;
-    }
-    int main(){
-        int arr[]={10,20,30,40,50,60};
-        int size=sizeof(arr)/sizeof(int);
-        int element=60;
-        int TernarySearch=TernaryFunction(arr,size,element);
-        printf("%d",TernarySearch);
-    }
 
+        int main()
+        {
+            int n;
+            scanf("%d", &n);
+            int arr[n];
+            for(int i=1; i<=n; i++)
+                scanf("%d", &arr[i]);
 
-
-
-
-
-
-
-
-
+         struct  Result result = linearSearchFunctional(arr, n);
+            for(int i=1; i<result.numberOfMatches; i++)
+            {
+                printf("%d ", result.indices[i]);
+            }
+            return  0;
+        }
